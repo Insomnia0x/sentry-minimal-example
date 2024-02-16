9e6f90d077a49bfe5b156ca4bbf2a993f2c80860
Insomnia0x <0xInsomnia@protonmail.com>
2024-02-16 12:10:14 -0500

    creates issue example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use sentry_tracing::EventFilter;
 use tracing_subscriber::prelude::*;
 
 fn main() {
@@ -15,7 +16,16 @@ fn main() {
 
     tracing_subscriber::registry()
         .with(tracing_subscriber::fmt::layer())
-        .with(sentry_tracing::layer().enable_span_attributes())
+        .with(
+            sentry_tracing::layer()
+                .event_filter(|md| match *md.level() {
+                    tracing::Level::ERROR => EventFilter::Exception,
+                    tracing::Level::INFO => EventFilter::Event,
+                    tracing::Level::DEBUG => EventFilter::Breadcrumb,
+                    _ => EventFilter::Ignore,
+                })
+                .enable_span_attributes(),
+        )
         .init();
 
     tokio::runtime::Builder::new_multi_thread()
@@ -23,48 +33,27 @@ fn main() {
         .build()
         .unwrap()
         .block_on(async {
-            // these do not create breadcrumbs in the transaction.
-            tracing_spans_example().await;
-            tracing_spans_example_with_manual_breadcrumb().await;
+            // creates an issue with breadcrumbs.
+            creates_issue_example().await;
         });
 }
 
 /// Example taken from the docs: https://github.com/getsentry/sentry-rust/tree/master/sentry-tracing#tracing-spans
 #[tracing::instrument]
-async fn tracing_spans_example() {
+async fn creates_issue_example() {
     for i in 0..10 {
-        tracing_spans_example_inner(i).await;
+        creates_issue_example_inner(i).await;
     }
+
+    // this creates the issue.
+    tracing::info!("completed");
 }
 
 // This creates spans inside the outer transaction, unless called directly.
 #[tracing::instrument]
-async fn tracing_spans_example_inner(i: u32) {
+async fn creates_issue_example_inner(i: u32) {
     // Also works, since log events are ingested by the tracing system
     tracing::debug!(number = i, "Generates a breadcrumb");
-
-    tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-}
-
-/// Example taken from docs: https://docs.sentry.io/platforms/rust/enriching-events/breadcrumbs/
-#[tracing::instrument]
-async fn tracing_spans_example_with_manual_breadcrumb() {
-    for i in 0..10 {
-        tracing_spans_example_inner_with_manual_breadcrumb(i).await;
-    }
-}
-
-// This creates spans inside the outer transaction, unless called directly.
-#[tracing::instrument]
-async fn tracing_spans_example_inner_with_manual_breadcrumb(i: u32) {
-    use sentry::{add_breadcrumb, Breadcrumb, Level};
-
-    add_breadcrumb(Breadcrumb {
-        category: Some("category".into()),
-        message: Some(format!("number {}", i)),
-        level: Level::Info,
-        ..Default::default()
-    });
 
     tokio::time::sleep(std::time::Duration::from_millis(100)).await;
 }
